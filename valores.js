fae42828ec2bd09dcb03cef2bfda78bd24395d39
Paulina Sienra Budnik <paulina.sienra@fing.edu.uy>
2022-02-26 15:57:17 -0300

    segundo commit

---
+++ valores.js
@@ -0,0 +1,11 @@
+40
+"paulina Sienra cadena de texto"
+
+true
+false
+
+null
+undefined//no usar
+
+[1,2,3]//array
+{nombre: Paulina}//objeto
---
+++ variables.js
@@ -0,0 +1,21 @@
+var nombre = "Paulina";
+
+//declarar
+var edad
+
+edad
+//inicializar
+edad = 30;
+
+edad
+
+var elementos = ["Computadora", "Celular"]
+elementos[1];//Computadora
+
+var persona = {
+    nombre: Paulina,
+    edad:30
+}//var compleja
+
+persona;
+ 
