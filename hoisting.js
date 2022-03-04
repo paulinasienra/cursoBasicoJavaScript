d3aeb06db8e8239fec3109e33b38f24ff6563c64
Paulina Sienra Budnik <paulina.sienra@fing.edu.uy>
2022-03-04 19:01:59 -0300

    hoisting y conditionals

---
+++ hoisting.js
@@ -0,0 +1,4 @@
+console.log(miNombre);
+
+miNombre = "Paulina";//JS me inicializa mi variable como var miNombre = undifined
+
---
+++ if.js
@@ -0,0 +1,9 @@
+var edad = 18;
+
+if (edad === 18) {
+    console.log("Tiene 18");
+} else if (edad > 18) {
+    console.log("Tienes mas de 18")
+} else {
+    console.log("No es mayor de 18")
+}
